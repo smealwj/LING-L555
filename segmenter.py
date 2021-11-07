5baaf274f3ee07f257a7800661ace989bef0a05f
smealwj <89487841+smealwj@users.noreply.github.com>
2021-11-07 16:02:46 -0500

    Delete segmenter.py

--- segmenter.py
+++
@@ -1,7 +0,0 @@
-import sys
-with open("wikiest.txt", "r") as read_file:
-	raw_data = read_file.read()
-re_data = raw_data.replace(".", ".\n")
-with open("segmenter.txt", "w") as save_for_gh:
-	save_for_gh.write(re_data)
-print(re_data)
